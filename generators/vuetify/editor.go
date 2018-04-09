42dba3ec73ca5050cc2101075e7045ee86bcd6f1
Yusuf Satar <yusuf@lsl.digital>
2018-04-09 22:00:27 +0400

    Vuetify List component

--- generators/vuetify/editor.go
+++ generators/vuetify/editor.go
@@ -9,7 +9,7 @@ import (
 	"github.com/fluxynet/gocipe/generators"
 )
 
-var tmplEditor, x = template.New("GenerateEditor").Parse(`
+var tmplEditor, _ = template.New("GenerateEditor").Parse(`
 <template>
     <div class="container">
         <h1>{{.Name}}</h1>
@@ -33,7 +33,7 @@ export default {
             return
         }
 
-        this.axios.get("{{.ServiceURL}}/{{.Endpoint}}/" + this.id).then(response => {
+        this.axios.get("/api/{{.Endpoint}}/" + this.id).then(response => {
             this.id = response.data.entity.id
             {{.Assignment}}
         })
@@ -42,7 +42,10 @@ export default {
         return {
             select: {
 				{{.SelectData}}
-            },
+			},
+			dates: {
+				{{.DateData}}
+			},
             messages: [],
             entity: {
                 {{.EntityDecl}}
@@ -55,7 +58,7 @@ export default {
     methods: {
         querySelections(fieldname, endpoint, filter, val) {
             this.select[fieldname].loading = true;
-            axios.get("{{.ServiceURL}}/" + endpoint + "?" + filter + "Lk=" + encodeURIComponent(val)).then(response => {
+            axios.get("/api/" + endpoint + "?" + filter + "Lk=" + encodeURIComponent(val)).then(response => {
                 this.select[fieldname].loading = false;
                 this.select[fieldname].items = response.data.entities.map(function(e) {
                     return { text: e[filter], value: e.id };
@@ -64,9 +67,9 @@ export default {
         },
         save() {
             if (this.id) {
-                axios.put("{{.ServiceURL}}/{{.Endpoint}}/" + this.id, this.entity).then(this.saved)
+                axios.put("/api/{{.Endpoint}}/" + this.id, this.entity).then(this.saved)
             } else {
-                axios.post("{{.ServiceURL}}/{{.Endpoint}}", this.entity).then(this.saved)
+                axios.post("/api/{{.Endpoint}}", this.entity).then(this.saved)
             }
 		},
 		saved(response) {
@@ -92,7 +95,7 @@ var tmplEditorCheckbox, _ = template.New("EditorCheckbox").Parse(`
 `)
 
 var tmplEditorDate, _ = template.New("EditorDate").Parse(`
-<v-date-picker v-model="entity.{{.Field}}" label="{{.Label}}" />
+<v-date-picker v-model="dates.{{.Field}}" label="{{.Label}}" @change="entity.created_at = dates.{{.Field}} + 'T00:00:00Z'"/>
 `)
 
 var tmplEditorSelectRel, _ = template.New("EditorSelectRel").Parse(`
@@ -118,6 +121,7 @@ func GenerateEditor(structInfo generators.StructureInfo) (string, error) {
 		fieldsMarkup     []string
 		selectData       []string
 		selectWatch      []string
+		dateData         []string
 		data             struct {
 			Assignment   string
 			EntityDecl   string
@@ -126,7 +130,7 @@ func GenerateEditor(structInfo generators.StructureInfo) (string, error) {
 			FieldsMarkup string
 			SelectData   string
 			SelectWatch  string
-			ServiceURL   string
+			DateData     string
 		}
 	)
 
@@ -136,7 +140,7 @@ func GenerateEditor(structInfo generators.StructureInfo) (string, error) {
 		)
 
 		if field.Name != "id" {
-			fieldsAssignment = append(fieldsAssignment, "            this.entity."+field.Name+" = response.data."+field.Name)
+			fieldsAssignment = append(fieldsAssignment, "            this.entity."+field.Name+" = response.data.entity."+field.Name)
 			fieldsEntityDecl = append(fieldsEntityDecl, field.Name+": null")
 		}
 
@@ -162,6 +166,7 @@ func GenerateEditor(structInfo generators.StructureInfo) (string, error) {
 				Label string
 				Field string
 			}{field.Widget.Label, field.Name})
+			dateData = append(dateData, field.Name+": null")
 		case "select-rel":
 			// if strings.HasPrefix(field.Widget.Options[0], "")
 			tmplEditorSelectRel.Execute(&markupSegment, struct {
@@ -192,7 +197,7 @@ func GenerateEditor(structInfo generators.StructureInfo) (string, error) {
 	data.FieldsMarkup = strings.Join(fieldsMarkup, "\n")
 	data.SelectData = strings.Join(selectData, ",\n")
 	data.SelectWatch = strings.Join(selectWatch, ",\n")
-	data.ServiceURL = "http://localhost:8888"
+	data.DateData = strings.Join(dateData, ",\n")
 
 	err = tmplEditor.Execute(&output, data)
 	if err == nil {
