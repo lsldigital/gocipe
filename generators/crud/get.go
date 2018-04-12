c6b6cc510fc2a58a802cb9023d4dc364eb45b5f6
Nadim Bundhoo <nadim@lsl.digital>
2018-04-12 17:18:29 +0400

    GenerateGetHook() + templates added/modified

--- generators/crud/get.go
+++ generators/crud/get.go
@@ -12,7 +12,12 @@ var tmplGet, _ = template.New("GenerateGet").Parse(`
 //Get returns a single {{.Name}} from database by primary key
 func Get(id int64) (*{{.Name}}, error) {
 	var entity = New()
-
+	{{if .PreExecHook }}
+    if entity, e := getPreExecHook(id); e != nil {
+		fmt.Printf("Error executing getPreExecHook() in Get(" + strconv.FormatInt(id, 10) + ") for entity '{{.Name}}': %s", e.Error())
+        return entity, e
+	}
+    {{end}}
 	rows, err := db.Query("SELECT {{.SQLFields}} FROM {{.TableName}} WHERE id = $1 ORDER BY id ASC", id)
 
 	if err != nil {
@@ -25,27 +30,50 @@ func Get(id int64) (*{{.Name}}, error) {
 		if err != nil {
 			return nil, err
 		}
+        {{if .PostExecHook }}
+        if entity, e := getPostExecHook(entity); e != nil {
+			fmt.Printf("Error executing getPostExecHook() in Get(" + strconv.FormatInt(id, 10) + ") for entity '{{.Name}}': %s", e.Error())
+			return entity, e
+        }
+        {{end}}
 		return entity, nil
 	}
-	
+
 	return nil, nil
 }
 `)
 
+var tmplGetHook, _ = template.New("GenerateGetHook").Parse(`
+{{if .PreExecHook }}
+func getPreExecHook(id int64) (*User, error) {
+	return New(), nil
+}
+{{end}}
+{{if .PostExecHook }}
+func getPostExecHook(entity *User) (*User, error) {
+	return entity, nil
+}
+{{end}}
+`)
+
 //GenerateGet generates code to get an entity from database
-func GenerateGet(structInfo generators.StructureInfo) (string, error) {
+func GenerateGet(structInfo generators.StructureInfo, PreExecHook bool, PostExecHook bool) (string, error) {
 	var output bytes.Buffer
 	data := new(struct {
 		Name         string
 		TableName    string
 		SQLFields    string
 		StructFields string
+		PreExecHook  bool
+		PostExecHook bool
 	})
 
 	data.Name = structInfo.Name
 	data.TableName = structInfo.TableName
 	data.SQLFields = ""
 	data.StructFields = ""
+	data.PreExecHook = PreExecHook
+	data.PostExecHook = PostExecHook
 
 	for _, field := range structInfo.Fields {
 		data.SQLFields += field.Name + ", "
@@ -56,7 +84,26 @@ func GenerateGet(structInfo generators.StructureInfo) (string, error) {
 	data.StructFields = strings.TrimSuffix(data.StructFields, ", ")
 
 	err := tmplGet.Execute(&output, data)
+	if err != nil {
+		return "", err
+	}
 
+	return output.String(), nil
+}
+
+// GenerateGetHook will generate 2 functions: getPreExecHook() and getPostExecHook()
+func GenerateGetHook(PreExecHook bool, PostExecHook bool) (string, error) {
+	var output bytes.Buffer
+
+	data := new(struct {
+		PreExecHook  bool
+		PostExecHook bool
+	})
+
+	data.PreExecHook = PreExecHook
+	data.PostExecHook = PostExecHook
+
+	err := tmplGetHook.Execute(&output, data)
 	if err != nil {
 		return "", err
 	}
