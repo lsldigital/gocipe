30679f359f61b1e93b04318af63c38313e5b9f61
Nadim Bundhoo <nadim@lsl.digital>
2018-04-12 17:12:39 +0400

    FileExists() utility function

--- generators/util.go
+++ generators/util.go
@@ -19,3 +19,12 @@ func GetAbsPath(location string) (string, error) {
 	location = path.Clean(wd + "/" + location)
 	return location, nil
 }
+
+// FileExists returns true is f (absolute path) exists; false otherwise
+func FileExists(f string) bool {
+	_, err := os.Stat(f)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return err == nil
+}
