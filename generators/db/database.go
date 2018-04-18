5ca4062549fd91ddf4944be510669ea5c3ff09dc
Yusuf Satar <yusuf@lsl.digital>
2018-04-18 18:10:09 +0400

    ManyMany for database

--- generators/db/database.go
+++ generators/db/database.go
@@ -2,8 +2,8 @@ package db
 
 import (
 	"bytes"
-	"html/template"
 	"strings"
+	"text/template"
 
 	"github.com/fluxynet/gocipe/generators"
 )
@@ -22,6 +22,17 @@ DROP TABLE IF EXISTS {{.TableName}};
 CREATE TABLE {{.TableName}} ({{range .Fields}}
 	"{{.Name}}" {{.Type}}{{.Constraints}}{{.Separator}}{{end}}
 );
+
+{{.ManyMany}}
+`)
+
+var tmplDatabaseManyMany, _ = template.New("GenerateDatabaseManyMany").Parse(`
+DROP TABLE IF EXISTS {{.TableName}};
+
+CREATE TABLE {{.TableName}} (
+	"{{.ThisID}}" INT NOT NULL,
+	"{{.ThatID}}" INT NOT NULL
+);
 `)
 
 //GenerateDatabase return code for database sql
@@ -29,10 +40,12 @@ func GenerateDatabase(structInfo generators.StructureInfo) (string, error) {
 	var (
 		output    bytes.Buffer
 		numFields = len(structInfo.Fields)
+		manymany  []string
 	)
 	data := new(struct {
 		TableName string
 		Fields    []tableField
+		ManyMany  string
 	})
 
 	data.TableName = structInfo.TableName
@@ -42,6 +55,13 @@ func GenerateDatabase(structInfo generators.StructureInfo) (string, error) {
 			field       tableField
 			constraints []string
 		)
+
+		if sfield.ManyMany != nil {
+			if strings.Compare(sfield.ManyMany.ThisID, sfield.ManyMany.ThatID) == -1 {
+				manymany = append(manymany, manymanyDatabase(sfield))
+			}
+			continue
+		}
 
 		field.Name = sfield.Name
 		field.Type = sfield.DBType
@@ -55,7 +75,7 @@ func GenerateDatabase(structInfo generators.StructureInfo) (string, error) {
 		}
 
 		if sfield.Default != "" {
-			constraints = append(constraints, "DEFAULT " + sfield.Default)
+			constraints = append(constraints, "DEFAULT "+sfield.Default)
 		}
 
 		if i < numFields-1 {
@@ -69,6 +89,10 @@ func GenerateDatabase(structInfo generators.StructureInfo) (string, error) {
 		data.Fields = append(data.Fields, field)
 	}
 
+	if len(manymany) > 0 {
+		data.ManyMany = strings.Join(manymany, "\n")
+	}
+
 	err := tmplDatabase.Execute(&output, data)
 
 	if err != nil {
@@ -76,4 +100,26 @@ func GenerateDatabase(structInfo generators.StructureInfo) (string, error) {
 	}
 
 	return output.String(), nil
+}
+
+func manymanyDatabase(field generators.FieldInfo) string {
+	var output bytes.Buffer
+	data := new(struct {
+		TableName string
+		ThisID    string
+		ThatID    string
+		ManyMany  string
+	})
+
+	data.TableName = field.ManyMany.PivotTable
+	data.ThisID = field.ManyMany.ThisID
+	data.ThatID = field.ManyMany.ThatID
+
+	err := tmplDatabaseManyMany.Execute(&output, data)
+
+	if err != nil {
+		return ""
+	}
+
+	return output.String()
 }
