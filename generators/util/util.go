1ff415cf317dfde6b1cda11f1fd11d20b451d073
mike <julesmichaelgiovanni@gmail.com>
2018-08-06 22:22:06 +0400

    - util files added

--- generators/util/util.go
+++ generators/util/util.go
@@ -12,5 +12,6 @@ func Generate(work util.GenerationWork, bootOpts util.BootstrapOpts) {
 	output.GenerateAndSave("Util", "util/credentials.go.tmpl", "util/credentials/credentials.gocipe.go", nil, false, false)
 	output.GenerateAndSave("Util", "util/util.go.tmpl", "util/util.gocipe.go", nil, false, false)
 	output.GenerateAndSave("Util", "util/web.go.tmpl", "util/web/web.gocipe.go", struct{ Bootstrap util.BootstrapOpts }{bootOpts}, false, false)
+	output.GenerateAndSave("Util", "util/files.go.tmpl", "util/files/files.gocipe.go", nil, false, false)
 	work.Waitgroup.Done()
 }
