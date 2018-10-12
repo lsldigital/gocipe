f9327efda81b29f6639abfbb9c4119dbe0427b53
Yusuf Satar <yusuf@lsl.digital>
2018-10-12 15:55:41 +0400

    grpc logger interceptor

--- generators/util/util.go
+++ generators/util/util.go
@@ -17,5 +17,6 @@ func Generate(work util.GenerationWork) {
 	output.GenerateAndSave("Util", "util/ws.go.tmpl", "util/web/ws.gocipe.go", nil, false)
 	output.GenerateAndSave("Util", "util/files.go.tmpl", "util/files/files.gocipe.go", data, false)
 	output.GenerateAndSave("Util", "util/imagist.go.tmpl", "util/imagist/imagist.gocipe.go", data, false)
+	output.GenerateAndSave("Util", "util/grpc.go.tmpl", "util/grpcx/grpc.gocipe.go", nil, false)
 	work.Waitgroup.Done()
 }
