1e0ef1ca7f98021ebd54da0917f09a5972e42bdf
Nadim Bundhoo <nadim@lsl.digital>
2018-11-16 10:34:29 +0400

    Generate deck package if deck.generate is true

--- generators/util/util.go
+++ generators/util/util.go
@@ -17,8 +17,10 @@ func Generate(out *output.Output, recipe *utils.Recipe) {
 	out.GenerateAndOverwrite("GenerateUtil Fileupload", "util/files.go.tmpl", "util/files/files.gocipe.go", output.WithHeader, data)
 	out.GenerateAndOverwrite("GenerateUtil Imagist", "util/imagist.go.tmpl", "util/imagist/imagist.gocipe.go", output.WithHeader, data)
 
-	out.GenerateAndOverwrite("GenerateUtil Decks", "util/decks.go.tmpl", "util/decks/decks.gocipe.go", output.WithHeader, struct {
-		AppImportPath string
-		Decks         []utils.DeckOpts
-	}{utils.AppImportPath, recipe.Decks.Decks})
+	if recipe.Decks.Generate {
+		out.GenerateAndOverwrite("GenerateUtil Decks", "util/decks.go.tmpl", "util/decks/decks.gocipe.go", output.WithHeader, struct {
+			AppImportPath string
+			Decks         []utils.DeckOpts
+		}{utils.AppImportPath, recipe.Decks.Decks})
+	}
 }
