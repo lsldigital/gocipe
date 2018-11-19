5c0f0eb514b138d4a747c363862267e8f0a8a595
Nadim Bundhoo <nadim@lsl.digital>
2018-11-19 18:12:17 +0400

    Implementation of Service Data (partly)

--- generators/data/data.go
+++ generators/data/data.go
@@ -17,7 +17,22 @@ func Generate(out *output.Output, r *util.Recipe) {
 		"Generate ServiceData Proto",
 		"data/service_data.proto.tmpl",
 		"proto/service_data.proto",
-		output.WithHeader, struct {
+		output.WithHeader,
+		struct {
+			Entities   []util.Entity
+			ImportPath string
+		}{
+			Entities:   r.Entities,
+			ImportPath: util.AppImportPath,
+		},
+	)
+
+	out.GenerateAndOverwrite(
+		"Generate ServiceData",
+		"data/service_data.go.tmpl",
+		"services/data/service_data.gocipe.go",
+		output.WithHeader,
+		struct {
 			Entities   []util.Entity
 			ImportPath string
 		}{
