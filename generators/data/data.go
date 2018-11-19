348e17f31d43e3be04b1fae48fc31b9ed62b6631
Nadim Bundhoo <nadim@lsl.digital>
2018-11-19 11:32:14 +0400

    Proto generation for Service Data

---
+++ generators/data/data.go
@@ -0,0 +1,28 @@
+package data
+
+import (
+	"github.com/fluxynet/gocipe/output"
+	"github.com/fluxynet/gocipe/util"
+)
+
+// Generate returns generated code for a Data service
+func Generate(out *output.Output, r *util.Recipe) {
+
+	// TODO: define in recipe / gocipe.json
+	// if !r.Data.Generate {
+	// 	return
+	// }
+
+	out.GenerateAndOverwrite(
+		"Generate ServiceData Proto",
+		"data/service_data.proto.tmpl",
+		"proto/service_data.proto",
+		output.WithHeader, struct {
+			Entities   []util.Entity
+			ImportPath string
+		}{
+			Entities:   r.Entities,
+			ImportPath: util.AppImportPath,
+		},
+	)
+}
