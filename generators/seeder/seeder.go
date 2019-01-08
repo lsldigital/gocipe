8db367a614e8e9b57751caae75fe5706c4a36518
Jeshta Bhoyedhur <jeshta@lsl.digital>
2019-01-08 14:02:58 +0400

    refactor code

--- generators/seeder/seeder.go
+++ generators/seeder/seeder.go
@@ -13,32 +13,22 @@ func Generate(r *util.Recipe) {
 
 	filename, err := util.GetAbsPath("schema/seeder.gocipe.sql")
 	if err != nil {
-		log.Fatal("Cannot get absolute path", err)
+		log.Println("Cannot get absolute path", err)
 	}
 	statements := util.GenerataSeeds(r)
 
 	//Delete file if already exists
-	exists(filename)
+	if _, err := os.Stat(filename); err == nil {
+		os.Remove(filename)
+	}
 
 	fi, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
-		log.Fatal("Cannot create f", err)
+		log.Println("Cannot create f", err)
 	}
 	defer fi.Close()
 
 	for _, s := range statements {
 		fmt.Fprintf(fi, s)
-	}
-}
-
-func exists(filename string) {
-
-	_, err := os.Stat(filename)
-
-	if !os.IsNotExist(err) {
-		err1 := os.Remove(filename)
-		if err1 != nil {
-			log.Println("File cannot be deleted: " + filename)
-		}
 	}
 }
